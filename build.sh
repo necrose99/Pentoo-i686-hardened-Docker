23646cf2836186824d2d64cd0e3a6913457b8ba8
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-11-12 22:34:40 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 suffix=$3 # e.g. -hardened
 arch=$1
 busybox_version=$2
-dist="http://distfiles.gentoo.org/releases/${arch}/autobuilds/"
-stage3="$(wget -q -O- ${dist}/latest-stage3-${busybox_version}${suffix}.txt | tail -n 1 | cut -f 1 -d ' ')"
+#dist="http://distfiles.gentoo.org/releases/${arch}/autobuilds/"
+#stage3="$(wget -q -O- ${dist}/latest-stage3-${busybox_version}${suffix}.txt | tail -n 1 | cut -f 1 -d ' ')"
 
 mkdir newWorldOrder; cd newWorldOrder
 echo "Downloading and extracting ${stage3}..."
-wget -q -c "${dist}/${stage3}"
-bunzip2 -c $(basename ${stage3}) | tar --exclude "./etc/hosts" --exclude "./sys/*" -xf -
-rm -f $(basename ${stage3})
+wget -q -c "ftp://pentoo.west.us.mirror.inerail.net/pentoo/Pentoo_x86_hardened/pentoo-x86-hardened-2015.0_RC4.2.iso"
+7z e -r pentoo-x86-hardened-2015.0_RC4.2.iso
+rm -f pentoo-x86-hardened-2015.0_RC4.2.iso
+sudo unsquashfs -f -d
 wget -q -O /busybox "http://www.busybox.net/downloads/binaries/latest/busybox-${busybox_version}"
 chmod +x /busybox
 /busybox rm -rf /lib* /usr /var /bin /sbin /opt /mnt /media /root /home /run /tmp
